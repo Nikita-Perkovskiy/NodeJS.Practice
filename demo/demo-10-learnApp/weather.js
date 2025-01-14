185665a456d02a3f8f8d7d3a88c36d23076bd2f4
Nikita Perkovskiy <nikitaperkovskij@gmail.com>
2025-01-14 23:16:57 +0100

    ls- work with file system

--- demo/demo-10-learnApp/weather.js
+++ demo/demo-10-learnApp/weather.js
@@ -5,12 +5,31 @@ import {
   printSuccess,
   printHelp,
 } from "./services/log.services.js";
+import { saveKeyValue } from "./services/storage.services.js";
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Success");
+  } catch (err) {
+    printError(err.message);
+  }
+};
 
 const initCLI = () => {
   const args = getArgs(process.argv);
-  console.log(args);
-  printError();
-  printSuccess();
-  printHelp();
+  // console.log(args);
+  // printError();
+  // printSuccess();
+  // printHelp();
+  if (args.h) {
+    printHelp();
+  } else if (args.s) {
+    //save city
+  } else if (args.t) {
+    //save token
+    return saveToken(args.t);
+  }
+  //print weather
 };
 initCLI();
