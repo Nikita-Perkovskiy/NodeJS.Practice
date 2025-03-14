b05e26ba09ec05a9cb8e440694373798d9a18c65
Nikita Perkovskiy <nikitaperkovskij@gmail.com>
2025-03-14 14:44:33 +0100

    add: error handlers

--- demo/demo-11-ExepressJS/src/app.ts
+++ demo/demo-11-ExepressJS/src/app.ts
@@ -3,25 +3,40 @@ import express, { Express } from "express";
 import { Server } from "http";
 import { LoggerService } from "./logger/logger.service.js";
 import { UserController } from "./users/user.controller.js";
+import { ExceptionFilter } from "./errors/exception.filter.js";
 
 export class App {
   app: Express;
   port: number;
   server?: Server;
   logger: LoggerService;
-  userController?: UserController;
-  constructor(logger: LoggerService, userController: UserController) {
+  userController: UserController;
+  exceptionFilter: ExceptionFilter;
+
+  constructor(
+    logger: LoggerService,
+    userController: UserController,
+    exceptionFilter: ExceptionFilter
+  ) {
     this.app = express();
     this.port = 8000;
-    this.logger = new LoggerService();
+    this.logger = logger;
     this.userController = userController;
+    this.exceptionFilter = exceptionFilter;
   }
+
   public async init() {
     this.useRoutes();
+    this.useExceptionFilters();
     this.server = this.app.listen(this.port);
-    this.logger.log(`Server is work - http://localhost:${this.port}`);
+    this.logger.log(`Server is running - http://localhost:${this.port}`);
   }
+
   useRoutes() {
-    this.app.use("/users", this.userController!.router);
+    this.app.use("/users", this.userController.router);
+  }
+
+  useExceptionFilters() {
+    this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
   }
 }
--- demo/demo-11-ExepressJS/src/main.ts
+++ demo/demo-11-ExepressJS/src/main.ts
@@ -1,10 +1,15 @@
 import { App } from "./app.js";
+import { ExceptionFilter } from "./errors/exception.filter.js";
 import { LoggerService } from "./logger/logger.service.js";
 import { UserController } from "./users/user.controller.js";
 
 async function bootstrap() {
   const logger = new LoggerService();
-  const app = new App(logger, new UserController(logger));
+  const app = new App(
+    logger,
+    new UserController(logger),
+    new ExceptionFilter(logger)
+  );
   await app.init();
 }
 
